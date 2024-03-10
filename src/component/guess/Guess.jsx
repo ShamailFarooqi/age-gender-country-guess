349956209bc140b73fcfe52b4a98b57ec1f4dccb
ShamailFarooqi <shumailfarooqi12@gmail.com>
2024-03-10 18:29:45 +0530

    country flags added

--- src/component/guess/Guess.jsx
+++ src/component/guess/Guess.jsx
@@ -3,6 +3,8 @@ import { notFound } from "next/navigation";
 import { useState } from "react";
 import useSWR from "swr";
 import { countryCodes } from "./countryCode";
+import styles from "./guess.module.css";
+import Image from "next/image";
 
 export default function Guess(){
 
@@ -42,17 +44,22 @@ export default function Guess(){
           <button>Search</button>
         </form>
         </div>
-        {data?.length >0 &&
-        <div>
-            <ul>
-                <li>{data[0].name} age is {data[0].age} yrs</li>
-                <li>{data[0].name} gender is{data[1].gender}</li>
-                <li> {data[0].name} country is{countryCodes[data[2]['country'][0].country_id][name]} with code {data[2]['country'][0].country_id}</li>
+        {data?.length>0 && data[1].name ?
+        <div  >
+            <ul className={styles.lDisplay}>
+                <li >{data[0].name} age is {data[0].age} yrs</li>
+                <li >{data[0].name} gender is {data[1].gender}</li>
+                <li> {data[0].name} country is {countryCodes[data[2]['country'][0].country_id]["name"]} with code {data[2]['country'][0].country_id}</li>
             
             
            
             </ul>
+            <Image   width="144"
+             height="108"
+             src={`https://flagcdn.com/144x108/${data[2]['country'][0].country_id.toLowerCase()}.png` } />
         </div>
+        :
+        <h2>Sorry Data limit reached</h2>
         }
         </div>
     )
